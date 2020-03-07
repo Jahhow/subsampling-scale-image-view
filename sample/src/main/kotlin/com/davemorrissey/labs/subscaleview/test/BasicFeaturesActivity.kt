f3a137e9053e3a4d7363c72d0de74c413754b8cb
涂 家 浩 <35959817+Jahhow@users.noreply.github.com>
2020-03-07 23:20:10 +0800

    Add Test App Switch Image Button

--- sample/src/main/kotlin/com/davemorrissey/labs/subscaleview/test/BasicFeaturesActivity.kt
+++ sample/src/main/kotlin/com/davemorrissey/labs/subscaleview/test/BasicFeaturesActivity.kt
@@ -1,14 +1,30 @@
 package com.davemorrissey.labs.subscaleview.test
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.pages_activity.*
 
 class BasicFeaturesActivity : AppCompatActivity() {
+    val images = listOf("sanmartino 2000x1334.jpg", "card.png", "card 125x100.png")
+    var index = 0
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pages_activity)
         actionBar?.title = getString(R.string.basic_title)
-        imageView.setImage("/android_asset/sanmartino.jpg")
+        switchImage()
+    }
+
+    fun switchImage(menuItem: MenuItem? = null) {
+        imageView.setImage("/android_asset/${images[index]}")
+        ++index
+        index %= images.size
     }
 }
